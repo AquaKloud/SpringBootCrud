b8614ddaeb38a7409232c41f0829110ddb3a936c
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-06-19 14:22:18 +0530

    Add files via upload

---
+++ 8) Get All Customer/src/main/java/com/example/Create/Customer/CustomerDTO/service/CustomerService.java
@@ -0,0 +1,19 @@
+package com.example.Create.Customer.CustomerDTO.service;
+
+import com.example.Create.Customer.CustomerDTO.dto.CustomerDTO;
+import com.example.Create.Customer.CustomerDTO.dto.request.CustomerUpdateDTO;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    public String saveCustomer(CustomerDTO customerDTO);
+
+    String updateCustomer(CustomerUpdateDTO customerUpdateDTO);
+
+    CustomerDTO getCustomerById(int customerId);
+
+    String deleteCustomer(int customerId);
+
+    List<CustomerDTO> getAllCustomer();
+}
