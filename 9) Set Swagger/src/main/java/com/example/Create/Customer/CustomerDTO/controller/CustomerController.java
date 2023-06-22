6ae7c60adf2b8077455deb8cc46475bdfa69a973
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-06-22 15:06:37 +0530

    Add files via upload

---
+++ 9) Set Swagger/src/main/java/com/example/Create/Customer/CustomerDTO/controller/CustomerController.java
@@ -0,0 +1,59 @@
+package com.example.Create.Customer.CustomerDTO.controller;
+
+
+import com.example.Create.Customer.CustomerDTO.dto.CustomerDTO;
+import com.example.Create.Customer.CustomerDTO.dto.request.CustomerUpdateDTO;
+import com.example.Create.Customer.CustomerDTO.service.CustomerService;
+import com.example.Create.Customer.CustomerDTO.service.IMPL.CustomerServiceIMPL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/customer")
+@CrossOrigin
+
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping(path = "/save-customer")
+    public String saveCustomer(@RequestBody CustomerDTO customerDTO){
+        //CustomerServiceIMPL customerService = new CustomerServiceIMPL();
+        customerService.saveCustomer(customerDTO);
+        return "saved";
+    }
+
+    @PutMapping(path = "/update-customer")
+    public String updateCustomer(@RequestBody CustomerUpdateDTO customerUpdateDTO){
+        String message = customerService.updateCustomer(customerUpdateDTO);
+        return message;
+    }
+
+    @GetMapping(
+            path = "/get-by-id",
+            params = "id"
+    )
+    public CustomerDTO getCustomerById(@RequestParam(value = "id") int customerId){
+        CustomerDTO customerDTO = customerService.getCustomerById(customerId);
+        return customerDTO;
+    }
+
+    @DeleteMapping(
+            path ="/delete-customer/{id}"
+    )
+    public String deleteCustomer(@PathVariable (value="id") int customerId){
+        String deleted = customerService.deleteCustomer(customerId);
+        return deleted;
+    }
+
+    @GetMapping(
+            path = "get-all-customers"
+    )
+    public List<CustomerDTO> getAllCustomer(){
+        List<CustomerDTO> customerDTOS = customerService.getAllCustomer();
+        return customerDTOS;
+    }
+}
---
+++ 9) Set Swagger/src/main/java/com/example/Create/Customer/CustomerDTO/repo/CustomerRepo.java
@@ -0,0 +1,11 @@
+package com.example.Create.Customer.CustomerDTO.repo;
+
+import com.example.Create.Customer.CustomerDTO.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface CustomerRepo extends JpaRepository<Customer, Integer> {
+}
