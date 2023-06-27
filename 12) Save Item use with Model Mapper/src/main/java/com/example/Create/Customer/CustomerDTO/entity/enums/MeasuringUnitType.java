9ed96c546af12e9dd62638d3c6e41f4e61466d43
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-06-27 11:03:03 +0530

    Add files via upload

---
+++ 12) Save Item use with Model Mapper/src/main/java/com/example/Create/Customer/CustomerDTO/entity/enums/MeasuringUnitType.java
@@ -0,0 +1,5 @@
+package com.example.Create.Customer.CustomerDTO.entity.enums;
+
+public enum MeasuringUnitType {
+    KILO_GRAM, LITTER, GRAM, MILLI_LITTER,Number;
+}
---
+++ 12) Save Item use with Model Mapper/src/main/java/com/example/Create/Customer/CustomerDTO/service/ItemService.java
@@ -0,0 +1,8 @@
+package com.example.Create.Customer.CustomerDTO.service;
+
+
+import com.example.Create.Customer.CustomerDTO.dto.request.ItemSaveRequestDTO;
+
+public interface ItemService {
+    String saveItem(ItemSaveRequestDTO itemSaveRequestDTO);
+}
